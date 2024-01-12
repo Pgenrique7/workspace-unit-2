3c96513a4e3d84f1917503cf568d960f5a2b7156
2DAW <2DAW@2DAM-19>
2024-01-12 14:45:16 +0100

    12/01/2024

--- ProyectoUnidad2/src/funciones_en_clase/EjerciciosRecursion.java
+++ ProyectoUnidad2/src/funciones_en_clase/EjerciciosRecursion.java
@@ -70,19 +70,125 @@ public class EjerciciosRecursion {
 		
 	}
 	
-	static void invertirNumero(int numero) {
+	private static int invertirNumero(int numero, int resultado) {
+		
+		
+		int digit=0;
+		
 		
 		if (numero==0) {
-			return;
+			return resultado;
 		}
 		
 		else {
 			
+			digit = numero%10;
+			resultado= digit + resultado*10;
+			numero=numero/10;
+			
+			
+			return invertirNumero (numero, resultado);
 			
 			
 		}
 		
 	}
+	
+	public static int sumaNPrimerosNumerosMejorado (int n) {
+		
+		
+		return sumaNPrimerosNumerosAux (n,0);
+	}
+	
+	
+	private static int sumaNPrimerosNumerosAux (int n, int acumulador) {
+		
+		if (n==0) {
+			
+			return acumulador;
+		}
+		
+		else {
+			
+			return sumaNPrimerosNumerosAux(n-1, acumulador+n);
+		}
+		
+	}
+	
+	public static int terminoFibonacci (int n) {
+		
+		if (n==0||n==1) {
+			
+			return 1;
+		}
+		else {
+			
+			return terminoFibonacci(n-1)+terminoFibonacci(n-2);
+		}
+		
+	}
+	//para casa hacer la version mejoradad de termino fibonacci con programacion dinamica y funcion auxiliar
+	
+	// para cassa hacer la version mejorada con programacion dinamica y funcion auxiliar de la suma de n 
+	// terminos fibonacci
+	
+	
+	//suponemos que a es mayor que b
+	public static int MCD (int a, int b) {
+		
+		if (b==0) {
+			
+			return a;
+		}
+		else {
+			
+			return MCD(b, a%b);
+		}
+		
+		
+	}
+	
+	
+	public static int MCDMejorado (int a, int b) {
+		
+		
+		if (a>=b) {
+			
+			return MCD (a,b);
+		}
+		
+		else {
+			
+			return MCD(b,a);
+		}
+	}
+	
+	
+	public static int maximoComunDivisor (int a, int b) {
+
+        int max, min, temp;
+
+        max = a>=b?a:b;
+        min= a<b?a:b;
+
+        while (min > 0) {
+            temp = min; 
+            min = max % min;
+            a = temp;
+        }
+        return a;
+
+    }
+	
+	public static int mcm (int a, int b) {
+		
+		
+		return a*b/maximoComunDivisor(a,b);
+		
+	}
+	
+	
+	
 	
 	public static void main(String[] args) {
 		
@@ -105,6 +211,9 @@ public class EjerciciosRecursion {
 		
 		System.out.println(contDigitos (1234));
 		
+		System.out.println();
+		
+		System.out.println(terminoFibonacci(6));
 
 	}
 
